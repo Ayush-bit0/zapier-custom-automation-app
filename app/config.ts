40e956d23afa20620a923c635139a9962eeaf886
Ayush-bit0 <118300324+Ayush-bit0@users.noreply.github.com>
2024-09-10 01:14:32 +0530

    Create config.ts

---
+++ app/config.ts
@@ -0,0 +1,6 @@
+export const BACKEND_URL = "https://zapier-backend-alp7.onrender.com"
+export const HOOKS_URL = "https://zapier-hooks-kfvs.onrender.com"
+
+
+
+// agr env na bnaye tb hume  ye config.ts m use krlo vo dikkt ni ni deta
